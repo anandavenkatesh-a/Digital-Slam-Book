c56c170dbf96df771d3af1929a91f41fdc07f217
AnandAVT <blackyvenky21@gmail.com>
2022-07-21 22:57:26 +0530

    setted up sass

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const port = 9999;
 
 const express_ejs_layouts = require('express-ejs-layouts');
 const path = require('path');
-///set up layout
-app.use(express_ejs_layouts);
-app.set('layout','./layouts/layout1');
-app.set("layout extractScripts", true);
-app.set('layout extractStyles',true);
+const sassMiddleware = require('node-sass-middleware');
+
+
+//set up sass
+app.use(sassMiddleware({
+    src:path.join(__dirname,'/assets/scss'),
+    dest:path.join(__dirname,'/assets/css'),
+    debug:true, //untill production stage it should be true
+    outputStyle:"expanded",
+    prefix:"/css"
+ }));
 
 //set up static-file midddleware
 app.use(express.static('./assets'));
@@ -18,6 +24,11 @@ app.use(express.static('./assets'));
 //set up template engine
 app.set('views',path.join(__dirname,'/templates'));
 app.set('view engine','ejs');
+//set up layout
+app.use(express_ejs_layouts);
+app.set('layout','./layouts/layout1');
+app.set("layout extractScripts", true);
+app.set('layout extractStyles',true);
 
 //set up routing
 app.use('/',require('./route'));
