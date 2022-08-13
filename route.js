960f38c985ea9f8dafecafbee1aab9ce9d2759e6
Venkatesh A <blackyvenky21@gmail.com>
2022-08-13 06:34:15 +0530

    add profile pic feature

--- route.js
+++ route.js
@@ -3,7 +3,19 @@
 const router = require('express').Router();
 const user_router = require('./user_ms/router');
 const comment_router = require('./comments_ms/routers/index');
-
+const multer = require('multer');
+const upload = multer({dest:'../uploads/user_profile'});
 router.use('/user',user_router);
 router.use('/comment',comment_router);
+
+
+router.post('/profile',upload.single('avatar'),(req,res) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.redirect('back');
+});
+router.get('/profile_pic_upload',(req,res) => {
+    res.render('upload_form');
+});
+
 module.exports = router;
